782421e17f38db73b749f1e2b8fa07a57c3aea88
Thomas Brockmeier <thomas.brockmeier@gmail.com>
2019-05-17 14:15:19 +0200

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,11 @@
+name := "Kafka101"
+
+version := "0.1"
+
+scalaVersion := "2.12.8"
+
+// https://mvnrepository.com/artifact/org.apache.kafka/kafka-clients
+libraryDependencies += "org.apache.kafka" % "kafka-clients" % "2.2.0"
+
+// https://mvnrepository.com/artifact/com.typesafe.scala-logging/scala-logging
+libraryDependencies += "com.typesafe.scala-logging" %% "scala-logging" % "3.9.2"
---
+++ src/main/scala/com/github/thomasbrockmeier/Kafka101/ProducerDemo.scala
@@ -0,0 +1,21 @@
+package com.github.thomasbrockmeier.Kafka101
+
+import java.util.Properties
+
+import org.apache.kafka.clients.producer.{KafkaProducer, ProducerConfig, ProducerRecord}
+
+object ProducerDemo extends App {
+
+  val properties = new Properties()
+  properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092")
+  properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+  properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringSerializer")
+
+  val producer = new KafkaProducer[String, String](properties)
+
+  val record = new ProducerRecord[String, String]("first_topic", "Hello, World!")
+  
+  producer.send(record)
+  producer.close()
+
+}
